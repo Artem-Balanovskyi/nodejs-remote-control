8b3019773cad095ab65994da11ddd938e670a4ab
Artem Balanovskyi <mr.balanovsky@gmail.com>
2023-01-23 22:17:03 +0200

    feat: implement screenshot capturing operation

--- src/comands/allComands.ts
+++ src/comands/allComands.ts
@@ -1,6 +1,7 @@
 import  { getMousePsn, mouseMoveUp, mouseMoveDown, mouseMoveRight, mouseMoveLeft } from "./mouse";
 import { IallCommands } from "../interfaces/allCommandsInterface";
 import { drawRectangle, drawCircle } from "./draw";
+import { makeScreenshot } from "./screenshot";
 
 
 export const allCommands: IallCommands = {
@@ -11,5 +12,6 @@ export const allCommands: IallCommands = {
 'mouse_left': mouseMoveLeft,
 'draw_square': drawRectangle,
 'draw_rectangle': drawRectangle,
-'draw_circle': drawCircle
+'draw_circle': drawCircle,
+'prnt_scrn': makeScreenshot
 }
--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,9 @@ const WS_PORT = 8080;
 
 makeMsgColoured(34, `Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+httpServer.on('close', () => makeMsgColoured(31, 'http server closed'));
+httpServer.on('error', (err) => makeMsgColoured(31, `http server error: ${err.message}`));
+httpServer.on('clientError', (err) => makeMsgColoured(31, `Client http error: ${err.message}`));
 
 const wss = new WebSocketServer({ port: WS_PORT });
 
@@ -19,7 +22,7 @@ wss.on('connection', (ws, req) => {
     const duplex = createWebSocketStream(ws, { encoding: 'utf-8', decodeStrings: false });
 
     duplex.on('data', async (message) => {
-        makeMsgColoured(33, `received: ${message}`);
+        makeMsgColoured(33, `Received: ${message}`);
 
         message = message.split(' ');
         const command = message[0];
@@ -28,7 +31,7 @@ wss.on('connection', (ws, req) => {
         if (command in allCommands) {
             try {
                 const result = await allCommands[command](args);
-                makeMsgColoured(32, `success: ${result}`);
+                makeMsgColoured(32, `Done: ${result.split(' ')[0]}`);
                 duplex.write(result);
             } catch (err: any) {
                 makeMsgColoured(31, `Operation "${command}" failed, error: "${err.message}"`);
@@ -40,17 +43,26 @@ wss.on('connection', (ws, req) => {
         makeMsgColoured(34, 'Connection closed');
     });
 
-    
     process.on('SIGINT', () => {
         makeMsgColoured(34, 'Closing websocket server...');
-        ws.close();
-        makeMsgColoured(34, 'Connection closed');
+        wss.clients.forEach((socket) => {
+            socket.close();
+            const state = socket.readyState;
+            if (state === 1 || state === 2) {
+                socket.terminate();
+                makeMsgColoured(34, `Connection closed`);
+            }
+        });
         wss.close();
-        makeMsgColoured(34, 'Websocket server stopped');
-        process.exit(0);
+        makeMsgColoured(34, 'Websocket server closed');
+        httpServer.close();
+        makeMsgColoured(34, 'http server closed');
+        process.exit(0)
     });
 });
 
 wss.on('close', () => {
-    makeMsgColoured(34, 'Websocket server stopped');
+    makeMsgColoured(34, 'Websocket server closed');
+    wss.on('error', (err) => makeMsgColoured(31, `Error: "${err.message}"`));
+
 });
