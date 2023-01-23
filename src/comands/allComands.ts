c6016580306098e93cec86dff98ed4c670c55fa8
Artem Balanovskyi <mr.balanovsky@gmail.com>
2023-01-23 14:43:45 +0200

    feat: implement draw operations

--- src/comands/allComands.ts
+++ src/comands/allComands.ts
@@ -1,5 +1,6 @@
-import  { getMousePsn, mouseMoveUp, mouseMoveDown, mouseMoveRight, mouseMoveLeft } from "./mouse"
-import { IallCommands } from "../interfaces/allCommandsInterface"
+import  { getMousePsn, mouseMoveUp, mouseMoveDown, mouseMoveRight, mouseMoveLeft } from "./mouse";
+import { IallCommands } from "../interfaces/allCommandsInterface";
+import { drawRectangle, drawCircle } from "./draw";
 
 
 export const allCommands: IallCommands = {
@@ -8,4 +9,7 @@ export const allCommands: IallCommands = {
 'mouse_down': mouseMoveDown,
 'mouse_right': mouseMoveRight,
 'mouse_left': mouseMoveLeft,
+'draw_square': drawRectangle,
+'draw_rectangle': drawRectangle,
+'draw_circle': drawCircle
 }
--- src/comands/mouse.ts
+++ src/comands/mouse.ts
@@ -13,21 +13,21 @@ export const mouseMove = async (dx: number, dy: number) => {
 
 export const mouseMoveUp = async (dy: number) => {
 	await mouseMove(0, -dy);
-	return `mouse_up`
+	return `mouse_up`;
 }
 
 export const mouseMoveDown = async (dy: number) => {
 	await mouseMove(0, +dy);
-	return `mouse_down`
+	return `mouse_down`;
 }
 
 export const mouseMoveRight = async (dx: number) => {
     await mouseMove(+dx, 0);
-    return `mouse_right`
+    return `mouse_right`;
 }
 
 export const mouseMoveLeft = async (dx: number) => {
 	await mouseMove(-dx, 0);
-	return `mouse_left`
+	return `mouse_left`;
 }
 
